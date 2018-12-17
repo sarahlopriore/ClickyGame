4eeedc6b566a6ca34a149c04a15b812b69877a7c
sarahlopriore <sarah.lopriore@gmail.com>
2018-12-17 18:52:23 -0500

    Updates

---
+++ precache-manifest.668458fbde9cec0838462902813f92a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "6a460fa549aac3d28dec",
+    "url": "/ClickyGame/static/js/main.6a460fa5.chunk.js"
+  },
+  {
+    "revision": "b80d22d50b648368dd04",
+    "url": "/ClickyGame/static/js/1.b80d22d5.chunk.js"
+  },
+  {
+    "revision": "6a460fa549aac3d28dec",
+    "url": "/ClickyGame/static/css/main.d4ffb214.chunk.css"
+  },
+  {
+    "revision": "8dbf505862299e4931c06aab6fec872b",
+    "url": "/ClickyGame/index.html"
+  }
+];
